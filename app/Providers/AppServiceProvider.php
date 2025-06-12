1261733f505b964655482dca3ddf4c535c721e5f
OneDeveloper27 <muhammad.alfin.firdaus27@gmail.com>
2025-06-12 10:39:11 +0800

    Implement Laravel Passport for API authentication and add necessary migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::tokensExpireIn(now()->addMinutes(110));
+        Passport::refreshTokensExpireIn(now()->addHours(2));
+        Passport::personalAccessTokensExpireIn(now()->addDays(30));
     }
 }
