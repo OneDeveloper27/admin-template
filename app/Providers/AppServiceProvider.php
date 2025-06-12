d9fd572e70b059907c56c8078c75b0f90a7ab93c
OneDeveloper27 <muhammad.alfin.firdaus27@gmail.com>
2025-06-12 10:39:11 +0800

    Add GitHub OAuth configuration and update dependencies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Event;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::tokensExpireIn(now()->addMinutes(110));
+        Passport::refreshTokensExpireIn(now()->addHours(2));
+        Passport::personalAccessTokensExpireIn(now()->addDays(30));
+        Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
+            $event->extendSocialite('github', \SocialiteProviders\GitHub\Provider::class);
+        });
     }
 }
