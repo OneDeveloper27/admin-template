58f6c6d206f7c161cb9f97e22fc39967a35bf030
OneDeveloper27 <muhammad.alfin.firdaus27@gmail.com>
2025-06-13 00:09:25 +0800

    Update configuration files for improved service integration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
 
         Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
             $event->extendSocialite('github', \SocialiteProviders\GitHub\Provider::class);
+            $event->extendSocialite('google', \SocialiteProviders\Google\Provider::class);
         });
     }
 }
